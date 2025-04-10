95c2a7aeceae1d4cffa1f40ae135f7c6ca8c72c4
Jars <maulidfajar163@gmail.com>
2025-04-10 18:59:18 +0700

    `Removed unused imports and commented code`

--- main.js
+++ main.js
@@ -1,20 +1,9 @@
+const TelegramBot = require("node-telegram-bot-api");
+
 const axios = require("axios"); // Import axios for HTTP requests
 // const cheerio = require("cheerio"); // Import cheerio for web scraping
-const TelegramBot = require("node-telegram-bot-api");
-// const AladhanAPI = "https://api.aladhan.com/v1"; // Import Aladhan API URL
 const jikanjs = require("@mateoaranda/jikanjs"); // Import JikanJS library
-// import OpenAI from "openai"; // Import OpenAI library
-// const client = new OpenAI();
 // var cron = require("node-cron"); // Import cron library
-// const zenquotesAPI = "https://zenquotes.io/api/quotes/"; // Import ZenQuotes API URL
-// const { Translate } = require("@google-cloud/translate").v2; // Import Google Cloud Translate library
-// const lyricsAPI = "https://api.lyrics.ovh/v1/artist/title"; // Import Lyrics API URL
-// const weatherAPI = "http://api.weatherapi.com/v1/"; // Import Weather API URL
-// const uselessfacts = { "GET https://uselessfacts.jsph.pl/random.json" } // Import Useless Facts API URL
-// const NewsAPI = require("newsapi"); // Import NewsAPI library
-// const newsapi = new NewsAPI("API_KEY");
-// To query /v2/top-headlines
-// All options passed to topHeadlines are optional, but you need to include at least one of them
 
 const token = "process.env.BOT_TOKEN";
 const options = {
